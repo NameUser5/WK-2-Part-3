27f70c78ff60c4010b9a5027c356d286503e0389
S Daut <92053137+NameUser5@users.noreply.github.com>
2022-02-04 11:26:21 +0000

    Cleanup

--- main.py
+++ main.py
@@ -13,9 +13,48 @@ print(total)
 total = 0
 for num in range(21):
     total = total + num
-    print(total)
+    print(total) # Wanted to see how the 'for' loop works.
 
 print(" ")
+
+import operator
+
+numbers = range(0,21)
+for num in numbers:
+  if num <0:
+    if num >= 20:
+      total = operator.add(total, num)
+  
+print(total)
+
+# My initial attempts (some are not as intuitive; clunky):
+
+twenty = [x for x in range(21) if x % 1 == 0]
+print(twenty)
+sum_20 = sum(twenty)
+print(sum_20)
+
+rnge = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+rngesum = sum(rnge)
+print(rnge)
+print(rngesum)
+
+rnge2 = []
+for x in range(50):
+  if x > 0:
+    if x < 21:
+      rnge2.append(x) 
+print(rnge2)
+sum_rnge2 = sum(rnge2)
+print(sum_rnge2)
+
+#Did this to see what .append really does:
+rnge3 = []
+for x in range(50):
+  if x > 0:
+    if x < 21:
+      rnge3.append(x) # So .append is a "collector" of sorts?
+print(rnge3)
 
 ###############################################
 
@@ -28,7 +67,6 @@ num1 = int(input("Pick a number. Any number. "))
 num2 = int(input("Pick another number. Again, any number will do. "))
 num3 = int(input("Pick a third number. "))
 num4 = int(input("Pick a fourth number. "))
-##ADD PRECISION TO FLOAT INSTEAD
 
 numbers = [num1, num2, num3, num4]
 numbers.sort()
@@ -55,10 +93,9 @@ title3 = title3.upper()
 input(title3)
 
 import operator
-num1 = int(input("Pick a number. Any number. "))
-num2 = int(input("Pick another number. Again, any number will do. "))
-##ADD PRECISION TO FLOAT INSTEAD
-total = operator.add(num1,num2)
+num1 = float(input("Pick a number. Any number. "))
+num2 = float(input("Pick another number. Again, any number will do. "))
+total = round(operator.add(num1,num2))
 
 print(f"Your total is {total}.")
 
@@ -69,45 +106,16 @@ print(f"Your total is {total}.")
 # total = sum(num1:int,num2:int)
 # print(numbers)
 
+
+
+
+
+
+
+
+# ADDITIONAL NOTES # (FOR REFERENCE LATER)
+
 # total = num1 + num2--> works, but doesn't use .add()
-
-
-
-
-
-
-
-
-# for every num in numbers:
-#   if 
-
-# if numbers < 
-# twenty = [x for x in range(21) if x % 1 == 0]
-# print(twenty)
-# sum_20 = sum(twenty)
-# print(sum_20)
-
-# rnge = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-# rngesum = sum(rnge)
-# print(rnge)
-# print(rngesum)
-
-# rnge2 = []
-# for x in range(50):
-#   if x > 0:
-#     if x < 21:
-#       rnge2.append(x)
-# print(rnge2)
-# sum_rnge2 = sum(rnge2)
-# print(sum_rnge2)
-
-
-# rnge3 = []
-# for x in range(50):
-#   if x > 0:
-#     if x < 21:
-#       rnge3.append(x)
-# print(rnge3)
 
 # #for x in rnge if x % 1 == 0:
 #  # rnge.append(x)
@@ -119,7 +127,7 @@ print(f"Your total is {total}.")
 
 
 # inrange = list(input("Enter five numbers, from one to nine. "))
-# newrange = tuple(inrange)
+# newrange = tuple(inrange)-> TUPLES CANT SPLIT!
 # #for x in newrange:
 #   #if x % 1 == 0:
 #    # newrange.append(x)
